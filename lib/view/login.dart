8f43aba56476feb23e77d2df24657fb4150dc658
preciousrose13 <saniyasyed130804@gmail.com>
2023-12-10 23:23:01 +0500

    day_1_1

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -15,136 +15,138 @@ class LoginPage extends StatelessWidget {
         ),
         child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              Container(
-                margin: EdgeInsets.only(bottom: 20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-                  image: DecorationImage(
-                    image: AssetImage("images/bg2.png"),
-                    fit: BoxFit.fill,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+          
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                    image: DecorationImage(
+                      image: AssetImage("images/bg2.png"),
+                      fit: BoxFit.fill,
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 100,
-                      width: 400,
-                    ),
-                    Text(
-                      "Welcome",
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      "HOME!",
-                      style: TextStyle(
-                        fontSize: 60,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Hello, this is my Store",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                  ],
-                ),
-              ),
-
-             
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(20),
                   child: Column(
                     children: [
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
+                      SizedBox(
+                        height: 100,
+                        width: 400,
+                      ),
+                      Text(
+                        "Welcome",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
                       ),
-                      SizedBox(height: 20),
-                      TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: 'Password',
+                      SizedBox(height: 5),
+                      Text(
+                        "HOME!",
+                        style: TextStyle(
+                          fontSize: 60,
+                          fontWeight: FontWeight.w900,
+                          color: Colors.white,
                         ),
                       ),
-
-
-                      SizedBox(height: 20),
-
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.pink),
-                          fixedSize: MaterialStateProperty.all(Size(100, 50)),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          "Login",
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-
                       SizedBox(height: 10),
                       Text(
-                        "Don't have an account?",
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 10),
-
-                      // Text(
-                      //   "Create Account",
-                      //   textAlign: TextAlign.center,
-                      //   style: TextStyle(
-                      //     fontSize: 18,
-                      //     fontWeight: FontWeight.bold
-                      //   ),
-                      // ),
-
-                      ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.white),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => RegisterPage()),
-                         );
-                        },
-                        child: Text(
-                          "Register Now",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black
-                            ),
+                        "Hello, this is my Store",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
                         ),
                       ),
-
-                      // SizedBox(height: 10),
+                      SizedBox(height: 20),
                     ],
                   ),
                 ),
-              ),
-            ],
+          
+               
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(bottom: Radius.circular(30)),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                          ),
+                        ),
+          
+          
+                        SizedBox(height: 20),
+          
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.pink),
+                            fixedSize: MaterialStateProperty.all(Size(100, 50)),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+          
+                        SizedBox(height: 10),
+                        Text(
+                          "Don't have an account?",
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 10),
+          
+                        // Text(
+                        //   "Create Account",
+                        //   textAlign: TextAlign.center,
+                        //   style: TextStyle(
+                        //     fontSize: 18,
+                        //     fontWeight: FontWeight.bold
+                        //   ),
+                        // ),
+          
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.white),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => RegisterPage()),
+                           );
+                          },
+                          child: Text(
+                            "Register Now",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black
+                              ),
+                          ),
+                        ),
+          
+                        // SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,11 +1,34 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'customappbar.dart';
+import 'package:practiceapp/view/login.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
+      body: AnimatedSplashScreen(
+        splash: Column(
+          children: [
+            Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.white,
+              size: 100,
+            ),
+            
+            Text(
+              "Pocket Mall",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),
+            ),
+          ],
+        ),
+        nextScreen: LoginPage(),
+        splashTransition: SplashTransition.rotationTransition,
+        backgroundColor: Colors.pink,
+        )
     );
   }
 }
